f8bbfc3b99b64f2399c73ca68d42c0f38aa148b1
Silikhe <silaswans@gmail.com>
2021-07-10 00:47:22 +0300

    feature(form):display form to user [Finish  #178813681]

--- src/app/Task.ts
+++ src/app/Task.ts
@@ -1,6 +1,6 @@
 export interface Task {
     id?: number;
     text: String;
-    Day: String;
+    day: String;
     reminder:boolean
 }
--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -4,19 +4,19 @@ export const TASKS: Task[] = [
     {
         id: 1,
         text: "Doctors appointment",
-        Day: "May 5th at 2.00am",
+        day: "May 5th at 2.00am",
         reminder:true,
     },
     {
         id: 2,
         text: "Meeting School",
-        Day: "May 6th at 2.00am",
+        day: "May 6th at 2.00am",
         reminder:true,
     },
     {
         id: 3,
         text: "Food Shopping",
-        Day: "May 7th at 2.00am",
+        day: "May 7th at 2.00am",
         reminder:false,
     },
  
