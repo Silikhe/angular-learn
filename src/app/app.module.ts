1ba9f1a8f8bd87c6ff69e3056df190bb63503607
Silikhe <silaswans@gmail.com>
2021-07-13 01:13:31 +0300

    Repairing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
-import { EvansComponent } from './components/evans/evans.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,6 @@ import { EvansComponent } from './components/evans/evans.component';
     ButtonComponent,
     TasksComponent,
     TasksItemComponent,
-    EvansComponent
   ],
   imports: [
     BrowserModule
